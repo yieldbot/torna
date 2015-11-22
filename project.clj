1ab5a6d793ad272f10609ab951782874fa9ab64a
Kunal Nawale <knawale@yieldbot.com>
2015-11-21 19:24:39 -0500

    bump version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject yieldbot/torna "0.1.1-SNAPSHOT"
+(defproject yieldbot/torna "0.1.2-SNAPSHOT"
   :description "Kafka batch processor library"
   :url "https://github.com/yieldbot/torna"
   :license {:name "Eclipse Public License"
